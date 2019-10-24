7dc7ad1be5dc2f2eac6bd13824533a0573994526
Nick Babb <nicholas.a.babb-1@ou.edu>
2019-10-23 21:12:48 -0500

    Added 2 different DateTimeOne constructors, implemented getValueOfSecond method

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,5 +1,19 @@
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
-   
+	private static final long MILLI_CONVERSION_FACTOR = 1000;
+	private long timeInMilliseconds;
+	
+	public DateTimeOne() {
+		timeInMilliseconds = System.currentTimeMillis();	
+	}
+	
+	public DateTimeOne(long input) {
+		timeInMilliseconds = input;
+	}
+	
+    public int getValueOfSecond() {
+	   return (int) (timeInMilliseconds / MILLI_CONVERSION_FACTOR);
+	   
+    }
 }
