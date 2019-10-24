de4246ee8ff5e8b4e68b2c6177637161db9499fe
Nick Babb <nicholas.a.babb-1@ou.edu>
2019-10-24 11:35:11 -0500

    Implemented dateTimeDifferentZone method

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,7 +1,7 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.TimeZone;
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
@@ -52,33 +52,59 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
 	@Override
 	public void dateTimeOfOtherCity() {
 		// Print Current Time in current time zone in military time
-		Calendar calendar = new GregorianCalendar();
+		Calendar calendarDefault = new GregorianCalendar();
 		SimpleDateFormat format = new SimpleDateFormat("kk:mm");
-		System.out.println("Time on Server: " + format.format(calendar.getTime()));
+		System.out.println("Time on Server: " + format.format(calendarDefault.getTime()));
 		
 		// Print Current Time in GMT, Greenwich Mean Time
-		TimeZone tz = TimeZone.getTimeZone("GMT");
+		TimeZone tz = TimeZone.getTimeZone("Greenwich Mean Time");
 		Calendar calendarGMT = new GregorianCalendar(tz);
-		format = new SimpleDateFormat("kk:mm");
 		System.out.println("GMT: " + format.format(calendarGMT.getTime()));
 		
 		//Print Current Time in BST, Bangladesh Standard Time
 		tz = TimeZone.getTimeZone("Bangladesh Standard Time");
 		Calendar calendarBST = new GregorianCalendar(tz);
-		format = new SimpleDateFormat("kk:mm");
 		System.out.println("BST (90E): " + format.format(calendarBST.getTime()));
 		
 		//Print Current Time in CST, Central Standard Time
 		tz = TimeZone.getTimeZone("Central Standard Time");
 		Calendar calendarCST = new GregorianCalendar(tz);
-		format = new SimpleDateFormat("kk:mm");
 		System.out.println("CST (90W): " + format.format(calendarCST.getTime()));
 	}
+	
+	// Sample Output:
+	// GMT: 10/08/2019 20:26
+	// BST: 10/09/2019 02:26
+	// CST: 10/08/2019 15:26
+	// Additionally, store these values in <String, String> hashmap. Time Zone is key,
+	// Date and time are value;
 
 	@Override
-	void dateTimeDifferentZone() {
-		// TODO Auto-generated method stub
+	public void dateTimeDifferentZone() {
+		// Hashmap to store the outputs
+		HashMap<String, String> timeZonesAndTime = new HashMap<String,String>();
 		
+		// Print Current Date and Time in GMT, Greenwich Mean Time, and store it in timeZonesAndTime
+		TimeZone tz = TimeZone.getTimeZone("Greenwich Mean Time");
+		Calendar calendarGMT = new GregorianCalendar(tz);
+		SimpleDateFormat format = new SimpleDateFormat("MM/DD/YYYY kk:mm");
+		String timeZoneGMT = format.format(calendarGMT.getTime());
+		System.out.println("GMT: " + timeZoneGMT);
+		timeZonesAndTime.put("GMT", timeZoneGMT);
+		
+		// Print Current Date and Time in BST, Bangladesh Standard Time, and store it in timeZonesAndTime
+		tz = TimeZone.getTimeZone("Bangladesh Standard Time");
+		Calendar calendarBST = new GregorianCalendar(tz);
+		String timeZoneBST = format.format(calendarBST.getTime());
+		System.out.println("BST: " + timeZoneBST);
+		timeZonesAndTime.put("BST", timeZoneBST);
+		
+		// Print Current Date and Time in CST, Central Standard Time, and store it in timeZonesAndTime
+		tz = TimeZone.getTimeZone("Central Standard Time");
+		Calendar calendarCST = new GregorianCalendar(tz);
+		String timeZoneCST = format.format(calendarCST.getTime());
+		System.out.println("BST: " + timeZoneCST);
+		timeZonesAndTime.put("CST", timeZoneBST);
 	}
 
 	@Override
