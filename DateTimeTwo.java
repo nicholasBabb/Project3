dcc967fe48ed4c32a84537bd0861e3e9ce6e9140
Nick Babb <nicholas.a.babb-1@ou.edu>
2019-10-24 11:00:15 -0500

    Imported necesseary method headers from other classes

--- DateTimeTwo.java
+++ DateTimeTwo.java
@@ -1,5 +1,46 @@
 
 public class DateTimeTwo extends MesoDateTimeOneAbstract
 {
+
+	public void compareYear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	int getValueOfSecond() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	void dateTimeNow() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void sleepForFiveSec() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void dateTimeOfOtherCity() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void dateTimeDifferentZone() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void timeZoneHashMap() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
