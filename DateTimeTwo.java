67feb422a016910d28fb312033ff71eaaa94fd97
Nick Babb <nicholas.a.babb-1@ou.edu>
2019-10-25 17:33:49 -0500

    Implemented daysOfCurrentMonth method

--- DateTimeTwo.java
+++ DateTimeTwo.java
@@ -1,46 +1,36 @@
+import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class DateTimeTwo extends MesoDateTimeOneAbstract
 {
+	
+	public void daysOfCurrentMonth() {
+		YearMonth thisMonth = YearMonth.now();
+		LocalDate tenthDayOfMonth = thisMonth.atDay(10);
+		String tenthDay = tenthDayOfMonth.getDayOfWeek().toString();
+		LocalDate eighteenthDayOfMonth = thisMonth.atDay(18);
+		String eighteenthDay = eighteenthDayOfMonth.getDayOfWeek().toString();
+		System.out.println("The tenth day of this month is " + tenthDay + " and eighteenth is " + eighteenthDay);
+		
+	}
+
+	public void daysOfAnyMonth(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	public void compareYear() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	int getValueOfSecond() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	void dateTimeNow() {
+	public void dateHashMap() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	void sleepForFiveSec() {
+	public void dateHashMapSorted() {
 		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	void dateTimeOfOtherCity() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	void dateTimeDifferentZone() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	void timeZoneHashMap() {
-		// TODO Auto-generated method stub
-		
-	}
-	
 }
